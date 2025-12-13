89a3a8febda6655b2b4c81d1a0aead310a3e3958
MOGESH.M <143895303+Mogeshaue@users.noreply.github.com>
2025-12-13 09:20:26 +0530

    server static files

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express=require('express')
 const sequelize=require('./config/database');
+const path=require('path');
 const app=express();
 const port=3000
 const cors=require('cors');
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from frontend folder
+app.use(express.static(path.join(__dirname, 'frontend')));
+
 const userRoutes=require('./routes/users');
 app.use('/user',userRoutes);
 app.use('/task',require('./routes/task'));
+
+// Serve index.html for root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
 
 
 sequelize.sync().
