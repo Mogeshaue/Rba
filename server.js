b30d9b7f4ba5e17569b61d84563f88e6827990f0
MOGESH.M <143895303+Mogeshaue@users.noreply.github.com>
2025-12-13 09:20:41 +0530

    remove comments

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const cors=require('cors');
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from frontend folder
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 const userRoutes=require('./routes/users');
 app.use('/user',userRoutes);
 app.use('/task',require('./routes/task'));
 
-// Serve index.html for root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
